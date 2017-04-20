a7adbe105aa73b6fab9583f3c9eb1ccb59270fbe
yoonju chang <yj.chang@tangramfactory.com>
2017-04-20 23:59:54 +0900

    .

--- pathfinder/quiz/views.py
+++ pathfinder/quiz/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 from django.views.generic import View
+from .models import Quiz, QuestionExample, Question, Exam, Explanations
 # Create your views here.
 
 
 
 @staff_member_required
 def create_quiz(request):
+    # exam = Exam()
+    # exam.question_id = 1
+    # exam.quiz_id = 1
+    # exam.save()
+    # # or
+    # Exam(quiz_id=1, question_id=1).save()
     pass
 
 
@@ -43,6 +50,18 @@ def show_question_list(request):
 
 @staff_member_required
 def show_question(request):
+    pass
+
+
+@staff_member_required
+def get_question_list(request):
+    '''
+    시험지의 수준에 해당하지만 현재 선택 된 적이 없는 문제들을 검색해서 반환한다. 
+    :param request: 
+    :return: 
+    '''
+    level = request.GET.get("level")
+    question_list =
     pass
 
 
