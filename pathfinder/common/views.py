6c22bcf42e77dd28cf60fe557c0a1287eb56e35c
yoonju chang <yj.chang@tangramfactory.com>
2017-06-04 21:55:53 +0900

    수정 작업

--- pathfinder/common/views.py
+++ pathfinder/common/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 
+from quiz.models import StudentScore
 # Create your views here.
 
 
@@ -18,9 +19,20 @@ class TopView(View):
         if request.user.is_authenticated:
             # 학생/선생님에 따라 대시보드가 다름
             if request.user.is_staff:
-                return render(request, 'manager-top.html', {})
+                latest_tested_student = StudentScore.objects.all()[:10]
+                return render(request, 'manager-top.html', {
+                    'tested_students': latest_tested_student
+                })
             else:
-                return render(request, 'student-top.html', {})
+                latest_tested_exam = request.user.student.studentscore_set.all()[:5]
+                # 참여하지 않은 테스트들 중에서 최근 등록 된 것을 보여준다.
+                exam_list = request.user.student.quiz_set.all().order_by('-pk')[:5]
+                # Quiz.objects.filter(students=request.user.student)
+
+                return render(request, 'student-top.html', {
+                    'tested_exam': latest_tested_exam,
+                    'exam_list': exam_list
+                })
         else:
             return render(request, 'top.html', {})
 
