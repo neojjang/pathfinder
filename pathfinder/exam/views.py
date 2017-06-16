11ff8b950f879b36ec4088c039e06474e1861fb9
yjfintalkdev <yoonju.chang@fintalk.co.kr>
2017-06-16 17:40:23 +0900

    수정

--- pathfinder/exam/views.py
+++ pathfinder/exam/views.py
@@ -1,11 +1,18 @@
+
+import logging
+from datetime import datetime
+
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 from django.views.generic import View
+from django.http import JsonResponse
 from quiz.models import Quiz
 # Create your views here.
 
+
+log = logging.getLogger(__name__)
 
 
 class ExamListView(LoginRequiredMixin, View):
@@ -27,26 +34,34 @@ class ExamListView(LoginRequiredMixin, View):
             'student': request.user.student
         })
 
-
-# @login_required
-# def view_list(request):
-#     pass
+@login_required
+def save_answer(request, exam_id=None):
+    '''
+    시험 각 문제의 답을 저장한다.
+    StudentAnswer
+    '''
+    data = {}
+    return JsonResponse(data)
 
 
 
 class TakeExamView(LoginRequiredMixin, View):
     def get(self, request, pk):
+        exam_key = datetime.strptime(datetime.today(), "%Y%m%d%H%M%S")
+        # 학생에게 배정된 시험만 풀 수 있다.
         exam_list = request.user.student.quiz_set.filter(pk=pk)
 
         exam = exam_list[0] if len(exam_list) > 0 else None
 
         return render(request, 'exam/take_exam.html', {
             'exam': exam,
+            'exam_key': exam_key,
             'total_questions': exam.questions.all().count() if exam else 0
         })
 
     def post(self, request):
-
+        log.debug(request.META)
+        log.debug(request.POST)
         return render(request, 'exam/take_exam.html', {
 
         })
