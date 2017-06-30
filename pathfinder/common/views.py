e7f1c9f0a977073a22a71a9d4582fa7b04a72dd7
yjfintalkdev <yoonju.chang@fintalk.co.kr>
2017-06-30 18:20:59 +0900

    수정

--- pathfinder/common/views.py
+++ pathfinder/common/views.py
@@ -1,5 +1,9 @@
+#-*- coding: utf-8 -*-
+import logging
+from datetime import datetime, timedelta
 from django.shortcuts import render
 from django.views import View
+from django.utils import timezone
 
 from quiz.models import StudentScore
 # Create your views here.
@@ -18,10 +22,22 @@ class TopView(View):
     def get(self, request, *args, **kwargs):
         if request.user.is_authenticated:
             # 학생/선생님에 따라 대시보드가 다름
+            this_month = today = datetime(year=datetime.today().year, month=datetime.today().month, day=1)
+            this_month = timezone.make_aware(this_month, timezone.get_current_timezone())
+            today = datetime(year=datetime.today().year, month=datetime.today().month, day=datetime.today().day)
+            today = timezone.make_aware(today, timezone.get_current_timezone())
             if request.user.is_staff:
-                latest_tested_student = StudentScore.objects.all()[:10]
+                this_month_tested_student = StudentScore.objects.filter(
+                    create_date__gt=this_month
+                )
+                today_tested_student = StudentScore.objects.filter(
+                    create_date__gt=today
+                )
+                latest_tested_student_10 = StudentScore.objects.all()[:10]
                 return render(request, 'manager-top.html', {
-                    'tested_students': latest_tested_student
+                    'this_month_tested_students': this_month_tested_student,
+                    'today_tested_students': today_tested_student,
+                    'latest_tested_students': latest_tested_student_10
                 })
             else:
                 latest_tested_exam = request.user.student.studentscore_set.all()
