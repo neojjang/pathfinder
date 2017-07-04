0dbfa2d79385f591e9950962b411a576e1d7c31b
yjfintalkdev <yoonju.chang@fintalk.co.kr>
2017-07-04 18:34:31 +0900

    수정

--- pathfinder/report/urls.py
+++ pathfinder/report/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
-from .views import dashboard, DetailView
+from .views import DashboardView, DetailView, StudentScoreView
 
 urlpatterns = [
-    url(r'^(\d+)/$', DetailView.as_view(), name='view_detail'),
-    url(r'^$', dashboard, name='dashboard'),
+    url(r'^(\d+)/detail/$', DetailView.as_view(), name='view_detail'),
+    url(r'^(\d+)/$', StudentScoreView.as_view(), name='view_score'),
+    url(r'^$', DashboardView.as_view(), name='dashboard'),
+
 ]
--- pathfinder/report/views.py
+++ pathfinder/report/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import View
+from django.db.models import Q
+from django.core.paginator import PageNotAnInteger, Paginator, EmptyPage
 
 from quiz.models import Quiz, StudentAnswer, StudentScore
+from quiz.views import StaffMemberRequiredMixin
 # Create your views here.
 
 
 
-class DetailView(LoginRequiredMixin, View):
+class StudentScoreView(LoginRequiredMixin, View):
     def get(self, request, exam_id):
         exam = get_object_or_404(Quiz, pk=exam_id)
 
@@ -17,13 +19,45 @@ class DetailView(LoginRequiredMixin, View):
             student=request.user.student
         ).order_by('-pk')
 
-        return render(request, 'report/detail_view.html', {
+        return render(request, 'report/score_view.html', {
             'exam': exam,
             'score_list': student_score
         })
 
 
+class DetailView(LoginRequiredMixin, View):
+    def get(self, request, score_id):
+        query = Q(pk=score_id)
+        if not request.user.is_staff:
+            query = query + Q(student=request.user.student)
+            template_file = 'report/detail_view.html'
+        else:
+            template_file = 'report/admin_detail_view.html'
 
-@login_required
-def dashboard(request):
-    pass
+        score = StudentScore.objects.get(query)
+
+        return render(request, template_file, {
+            'score': score,
+            'exam': score.quiz,
+
+        })
+
+
+class DashboardView(StaffMemberRequiredMixin, View):
+    def get(self, request):
+        page = request.GET.get('p')
+
+        tested_students = StudentScore.objects.all()
+        paginator = Paginator(tested_students, 20)
+        try:
+            tested_students = paginator.page(page)
+        except PageNotAnInteger:
+            tested_students = paginator.page(1)
+        except EmptyPage:
+            tested_students = paginator.page(paginator.num_pages)
+
+        return render(request, 'report/dashboard.html', {
+            'students': tested_students,
+        })
+
+
